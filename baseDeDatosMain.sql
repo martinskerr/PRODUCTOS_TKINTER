5d78796e4324fb70494ac72fadf75f483c4dfb3f
Martin Lopez <martinxx442@gmail.com>
2023-01-16 22:23:07 -0300

    baseDeDatosArreglada

--- baseDeDatosMain.sql
+++ baseDeDatosMain.sql
@@ -1,20 +1,20 @@
 CREATE TABLE PRODUCTOS(
     
-    productos_id number(55) not null,
     tipo_producto varchar2(55) not null,
-    expiracion number(55) not null,
-    numero_serie number (255) not null,
+    expiracion number(10) not null,
+    numero_serie number (10) not null,
 
-    CONSTRAINT PRIMARY_KEY_PRODUCTO PRIMARY KEY productos_id
+    CONSTRAINT PRIMARY_KEY_PRODUCTO PRIMARY KEY (numero_serie)
 
 );
 
 CREATE TABLE MERMA(
+    
+    nombre_Producto varchar(55) not null,
+    tipo_merma varchar2(55) null,
+    expiracion_merma number(10) not null,
+    numero_serie number(10) not null,
 
-    tipo_merma varchar2(55) not null,
-    expiracion_merma number(55) not null,
-    tipo_producto varchar2(55) not null,
-
-    CONSTRAINT PRIMARY_KEY_MERMA PRIMARY KEY tipo_merma,
-    CONSTRAINT FK_PRODUCTOS FOREIGN KEY tipo_producto
+    CONSTRAINT PRIMARY_KEY_MERMA PRIMARY KEY(nombre_Producto),
+    CONSTRAINT FK_PRODUCTOS FOREIGN KEY (numero_serie) REFERENCES PRODUCTOS(numero_serie)
 );
