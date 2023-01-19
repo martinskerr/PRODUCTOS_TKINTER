4e19dc24cf1b2f28f0d0827df97e4ee1ce9690d1
Martin Lopez <martinxx442@gmail.com>
2023-01-19 17:32:53 -0300

    condicionales de los datos aplicados

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 from tkinter import messagebox
 import csv
 import os
+from datetime import datetime
 #---------------------------------------------------Imports-----------------------------------!!
 
 #---------------------------------------------------TABLAS BASE DE DATOS-----------------------------------!!
@@ -28,6 +29,13 @@ c.execute("""
 #-----------------------------------creacion interfaz grafica con tkinter-----------------------------------!!
 root = Tk()
 root.title('BASE DE DATOS: INGRESO PRODUCTOS')
+root.geometry("1280x720+300+200")
+
+
+#tiempo actual
+def tiempo_actual():
+    current_date = datetime.now().date()
+    return current_date
 
 def renderProductos():
     rows = c.execute("SELECT * FROM PRODUCTOS").fetchall()
@@ -37,6 +45,21 @@ def renderProductos():
     for row in rows:
         tree.insert('', END, row[0], values=(row[1], row[2], row[3], row[4], row[5]))
 
+def generador_excel():
+    # Datos a escribir en el archivo CSV
+    data = [['Nombre Producto','Fecha de ingreso','Cantidad','Expiracion', 'Tipo Producto'],
+            ['nombre'],['fecha_de_ingreso'], ['cantidad'], ['expiracion'], ['tipoProducto']
+            ]
+
+    # Abre el archivo en modo escritura
+    with open('data.csv', 'w', newline='') as file:
+        # Crea un objeto CSV writer
+        csv_writer = csv.writer(file)
+        # Escribir cada fila de datos en el archivo
+        for row in data:
+            csv_writer.writerow(row)
+            print(row)
+    os.startfile('data.csv')
 
 def insertar(productos):
     c.execute("""
@@ -52,34 +75,34 @@ def nuevo_producto():
     def guardar():
         if not nombre.get():
             messagebox.showerror("Error", "el nombre es obligatorio")
-            return
-
-        if not fecha_de_ingreso.get():
-            messagebox.showerror("Error", "la fecha de ingreso del producto es obligatorio")
-            return            
-            
-        if not cantidad.get():
-            messagebox.showerror("Error", "la cantidad de productos es obligatoria")
-            return
+            return       
 
         if not expiracion.get():
             messagebox.showerror("Error", "la fecha de expiracion del producto es obligatoria.")
             return
 
+
         if not tipoProducto.get():
             messagebox.showerror("Error", "el tipo de producto es obligatorio")
             return
 
-        productos = {
-            'nombre': nombre.get(),
-            'fecha_de_ingreso': fecha_de_ingreso.get(),
-            'expiracion': expiracion.get(),
-            'tipoProducto': tipoProducto.get(),
-            'cantidad': cantidad.get(),           
-            
-        }
-        insertar(productos)
-        top.destroy()
+        try:
+            # convertir la cadena de texto en un objeto de fecha
+            expiracion_date = datetime.strptime(expiracion.get(), "%d/%m/%Y")
+            fecha_expiracion = expiracion_date.date()
+
+            productos = {
+                'nombre': nombre.get(),
+                'fecha_de_ingreso': tiempo_actual(),
+                'expiracion': expiracion_date,
+                'tipoProducto': tipoProducto.get(),
+                'cantidad': cantidad.get(),           
+            }
+            insertar(productos)
+            top.destroy()
+        except ValueError:
+            # Mostrar un mensaje de error
+            messagebox.showerror("Error", "El formato de fecha de expiracion es incorrecto. Debe ser dd/mm/yyyy.")
 
     top = Toplevel()
     top.title("Nuevo Producto")
@@ -90,9 +113,7 @@ def nuevo_producto():
     nombre.grid(row=0, column=1)
 
     lfecha_de_ingreso = Label(top, text='Fecha de ingreso')
-    fecha_de_ingreso = Entry(top, width=40)
     lfecha_de_ingreso.grid(row=1, column=0)
-    fecha_de_ingreso.grid(row=1, column=1)
 
     lexpiracion = Label(top, text='Expiracion')
     expiracion = Entry(top, width=40)
@@ -129,25 +150,6 @@ def eliminar_producto():
         renderProductos()
     else:
         pass
-
-def generador_excel():
-    # Datos a escribir en el archivo CSV
-    data = [[],
-            [],
-            [],
-            []]
-
-    # Abre el archivo en modo escritura
-    with open('data.csv', 'w', newline='') as file:
-        # Crea un objeto CSV writer
-        csv_writer = csv.writer(file)
-        # Escribir cada fila de datos en el archivo
-        for row in data:
-            csv_writer.writerow(row)
-            print(row)
-    os.startfile('data.csv')
-
-
 
 
 btn = Button(root, text='Nuevo Producto', command=nuevo_producto)
