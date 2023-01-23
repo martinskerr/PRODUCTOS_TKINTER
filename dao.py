ec47a8f592ab7b4dc7cad76d1afaea4f7cff37cf
Martin Lopez <martinxx442@gmail.com>
2023-01-23 16:58:55 -0300

    funcion main creada

---
+++ dao.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+#---------------------------------------------------TABLAS BASE DE DATOS-----------------------------------!!
+
+conn = sqlite3.connect('crm.db')
+c = conn.cursor()
+c.execute("""
+
+    CREATE TABLE if not exists PRODUCTOS(
+        id integer PRIMARY KEY AUTOINCREMENT,
+        nombre_producto TEXT NOT NULL,
+        fecha_de_ingreso TEXT NOT NULL,
+        cantidad_productos INTEGER NOT NULL,
+        expiracion INTEGER NOT NULL,
+        tipo_producto TEXT NOT NULL
+    )
+""")
+
+#---------------------------------------------------TABLAS BASE DE DATOS-----------------------------------!!
